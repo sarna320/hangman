03cc471261d97e9f7162932695526fc0ba927ccd
sarna320 <sarnacki99@wp.pl>
2023-10-27 22:37:04 +0200

    better user experience

--- hangman.py
+++ hangman.py
@@ -1,25 +1,25 @@
 import random
 import hangman_stages
+import hangman_words
 
-word_list = ["aardvark", "baboon", "camel"]
-
+print(hangman_stages.logo)
 # chosen_word=word_list[random.randint(0,len(word_list)-1)]
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(hangman_words.word_list)
 display = ["_"] * len(chosen_word)
-lives=6
-while "_" in display and lives>0:
+lives = 6
+while "_" in display and lives > 0:
     guess = input("Guess a letter: ").lower()
-    was_found=False
+    was_found = False
 
     for i in range(0, len(chosen_word)):
         if chosen_word[i] == guess:
             display[i] = guess
-            was_found=True
-    if(was_found==False):
-        lives-=1
+            was_found = True
+    if was_found == False:
+        lives -= 1
     print(f"{' '.join(display)}")
     print(hangman_stages.stages[lives])
-if(lives>0):
+if lives > 0:
     print("You win")
 else:
     print("You lose")
